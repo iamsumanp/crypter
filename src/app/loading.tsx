a8269f75edafe3d3961e043237f2866dc93f40d3
suporl <sumanpok9@gmail.com>
2023-02-07 00:55:04 +0545

    data fetched and displayed

---
+++ src/app/loading.tsx
@@ -0,0 +1,6 @@
+// import Skeleton from "react-loading-skeleton";
+// import "react-loading-skeleton/dist/skeleton.css";
+export default function Loading() {
+  // You can add any UI inside Loading, including a Skeleton.
+  return <div>loading...</div>;
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
+/* eslint-disable @next/next/no-img-element */
 import { cryptoData } from "@/data/data";
 import Image from "next/image";
+import { Suspense } from "react";
+import { CLIENT_RENEG_LIMIT } from "tls";
+// import Loading from "./loading";
 
 interface cryptoDataType {
   uuid: string;
@@ -15,51 +19,76 @@ interface cryptoDataType {
   rank: number;
 }
 
-export default function Home() {
+const getCoins = async () => {
+  const options = {
+    method: "GET",
+
+    headers: {
+      "X-RapidAPI-Key": "ea4afc88f8mshe9c07843689f0f4p153de1jsn2cdc8454bc6f",
+      "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
+    },
+  };
+
+  const res = await await fetch(
+    "https://coinranking1.p.rapidapi.com/coins?referenceCurrencyUuid=yhjMzLPhuIDl&timePeriod=24h&tiers%5B0%5D=1&orderBy=marketCap&orderDirection=desc&limit=50&offset=0",
+    options
+  ).then((resp) => resp.json());
+
+  return res.data.coins;
+};
+
+export default async function Home() {
+  const coinsData = await getCoins();
+
   return (
     <div className="flex flex-col space-y-6  w-full">
       <span className="text-2xl">Crypto rankings in the world</span>
+      {/* <Suspense fallback={<Loading />}> */}
       <div className="grid 	grid-cols-1 md:grid-cols-2 lg:grid-cols-3 text-xs md:text-base  gap-6  ">
-        {cryptoData.map((data: cryptoDataType, id) => (
-          <div key={id} className="bg-dark p-4 rounded-md  flex flex-col">
-            <div className="flex flex-col md:flex-row justify-between pb-10">
-              <span>
-                {id + 1}.{data.name}
-              </span>
-              <Image
-                src={data.iconUrl}
-                alt="cryptIcon"
-                height={20}
-                width={20}
-              ></Image>
+        {coinsData
+          .filter((data: any) => data.rank < 10)
+          .map((data: cryptoDataType, id: any) => (
+            <div key={id} className="bg-dark p-4 rounded-md  flex flex-col">
+              <div className="flex flex-col md:flex-row justify-between pb-10">
+                <span>
+                  {id + 1}.{data.name}
+                </span>
+                <Image
+                  src={data.iconUrl}
+                  alt="cryptIcon"
+                  height={20}
+                  width={20}
+                ></Image>
+                {/* <span>hello</span> */}
+              </div>
+              <div className="flex justify-between items-center pb-3">
+                <span>marketCap:</span>
+                <span className="text-gray-400 text-xs md:text-sm">
+                  {data.marketCap}
+                </span>
+              </div>
+              <div className="flex justify-between items-center pb-3">
+                <span>price:</span>
+                <span className="text-gray-400 text-xs md:text-sm">
+                  {parseFloat(data.price).toFixed(2)}
+                </span>
+              </div>
+              <div className="flex justify-between items-center pb-3">
+                <span>listed at:</span>
+                <span className="text-gray-400 text-xs md:text-sm">
+                  {data.listedAt}
+                </span>
+              </div>
+              <div className="flex justify-between items-center pb-3">
+                <span>change:</span>
+                <span className="text-gray-400 text-xs md:text-sm">
+                  {data.change}
+                </span>
+              </div>
             </div>
-            <div className="flex justify-between items-center pb-3">
-              <span>marketCap:</span>
-              <span className="text-gray-400 text-xs md:text-sm">
-                {data.marketCap}
-              </span>
-            </div>
-            <div className="flex justify-between items-center pb-3">
-              <span>price:</span>
-              <span className="text-gray-400 text-xs md:text-sm">
-                {parseFloat(data.price).toFixed(2)}
-              </span>
-            </div>
-            <div className="flex justify-between items-center pb-3">
-              <span>listed at:</span>
-              <span className="text-gray-400 text-xs md:text-sm">
-                {data.listedAt}
-              </span>
-            </div>
-            <div className="flex justify-between items-center pb-3">
-              <span>change:</span>
-              <span className="text-gray-400 text-xs md:text-sm">
-                {data.change}
-              </span>
-            </div>
-          </div>
-        ))}
+          ))}
       </div>
+      {/* </Suspense> */}
     </div>
   );
 }
